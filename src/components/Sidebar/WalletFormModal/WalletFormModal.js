e2772399784f082eac4f2cbf18e85e1a5b854204
ptienchuan <ptienchuan@gmail.com>
2020-01-04 10:46:50 +0700

    Add validation for wallet form modal

--- src/components/Sidebar/WalletFormModal/WalletFormModal.js
+++ src/components/Sidebar/WalletFormModal/WalletFormModal.js
@@ -3,22 +3,32 @@ import React, { useState, useContext } from "react";
 import Modal from "../../Modal/Modal";
 import Header from "../../Modal/Header/Header";
 import Body from "../../Modal/Body/Body";
-
 import Button from "../../UI/Button/Button";
 import Input from "../../UI/Input/Input";
-
 import WalletContext from "../../../contexts/WalletContext";
+import { validName } from "../../../validator";
 
 const WalletFormModal = props => {
-	const [wallet, setWallet] = useState({ name: "" });
-	const changeInputHandler = e => {
+	const [wallet, _setWallet] = useState({ name: "" });
+	const [nameError, _setNameError] = useState("");
+	const { addWallet } = useContext(WalletContext);
+
+	const _changeInputHandler = e => {
 		const newWallet = { ...wallet };
-		newWallet.name = e.target.value;
-		setWallet(newWallet);
+		const { value } = e.target;
+		// update state
+		newWallet.name = value;
+		_setWallet(newWallet);
+		// validation
+		const err = validName(value, "Name");
+		if (err !== nameError) {
+			_setNameError(err);
+		}
 	};
 
-	const { addWallet } = useContext(WalletContext);
-	const submitHandler = e => {
+	const _validForm = () => (validName(wallet.name) ? false : true);
+
+	const _submitHandler = e => {
 		e.preventDefault();
 		if (wallet.name.trim() !== "") {
 			addWallet(wallet);
@@ -30,7 +40,7 @@ const WalletFormModal = props => {
 		<Modal>
 			<Header title="Wallet" onClickButton={props.onClose} />
 			<Body>
-				<form onSubmit={submitHandler}>
+				<form onSubmit={_submitHandler}>
 					<Input
 						element="text"
 						label="Name"
@@ -39,12 +49,17 @@ const WalletFormModal = props => {
 						name="name"
 						autoComplete="off"
 						value={wallet.name}
-						onChange={changeInputHandler}
-						required
+						onChange={_changeInputHandler}
+						error={nameError}
 					/>
 
 					<div style={{ textAlign: "center", marginTop: "15px" }}>
-						<Button color="success">Regist</Button>
+						<Button
+							color="success"
+							disabled={_validForm() ? "" : "disabled"}
+						>
+							Regist
+						</Button>
 					</div>
 				</form>
 			</Body>
